acbc1bc6aa2cdfeb13e4ad7d13e29f8e27a5b7b7
mhcferreira <matheushenriquecoelhoferreira@gmail.com>
2019-09-23 11:59:42 -0300

    v3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AngularFireDatabaseModule} from '@angular/fire/database';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CadastroPage } from './contato/cadastro/cadastro.page';
